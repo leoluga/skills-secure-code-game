34e63a1ff602ded149653382ebadda1407cfef89
LeonardoLugarini <lugaleonardo@gmail.com>
2024-06-11 00:24:50 -0300

    level 1 complete

--- Season-1/Level-1/code.py
+++ Season-1/Level-1/code.py
@@ -1,33 +1,40 @@
 '''
-Welcome to Secure Code Game Season-1/Level-1!
-
-Follow the instructions below to get started:
-
-1. tests.py is passing but code.py is vulnerable
-2. Review the code. Can you spot the bug?
-3. Fix the code but ensure that tests.py passes
-4. Run hack.py and if passing then CONGRATS!
-5. If stuck then read the hint
-6. Compare your solution with solution.py
+Solution for level 1
 '''
 
 from collections import namedtuple
+from decimal import Decimal
 
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
+MAX_ITEM_AMOUNT = 100000
+MAX_QUANTITY = 100 
+MIN_QUANTITY = 0 
+MAX_TOTAL = 1e6 
+
 def validorder(order: Order):
-    net = 0
+    payments = Decimal('0')
+    expenses = Decimal('0')
 
     for item in order.items:
         if item.type == 'payment':
-            net += item.amount
+            if (-MAX_ITEM_AMOUNT <= item.amount) | (item.amount <= MAX_ITEM_AMOUNT):
+                payments += Decimal(str(item.amount))
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            int_condition = isinstance(item.quantity, int)
+            quantity_condition = (MIN_QUANTITY < item.quantity) or (item.quantity <= MAX_QUANTITY)
+            amount_condition = (MIN_QUANTITY < item.amount) or (item.amount <= MAX_ITEM_AMOUNT)
+
+            if  int_condition and quantity_condition and amount_condition:
+                expenses += Decimal(str(item.amount)) * item.quantity
         else:
             return "Invalid item type: %s" % item.type
+    
+    if abs(payments) > MAX_TOTAL or expenses > MAX_TOTAL:
+        return "Total amount payable for an order exceeded"
 
-    if net != 0:
-        return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net)
+    if payments != expenses:
+        return "Order ID: %s - Payment imbalance: $%0.2f" % (order.id, payments - expenses)
     else:
         return "Order ID: %s - Full payment received!" % order.id
